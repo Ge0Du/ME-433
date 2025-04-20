51dad2e2b20f5029e2ad389dd712a4d1ea017f76
Ge0Du <147010534+Ge0Du@users.noreply.github.com>
2025-04-19 23:32:19 -0500

    HW3 Completed

--- Template/Template.c
+++ Template/Template.c
@@ -9,25 +9,29 @@ int main() {
         sleep_ms(100);
     }
     printf("Start!\n");
-    gpio_init(12); // LED PIN NEED TO UPDATE TO IRL
-    gpio_init(13); // BUTTON PIN NEED TO UPDATE TO IRL
-    gpio_set_dir(12, GPIO_OUT);
-    gpio_set_dir(13, GPIO_IN);
+    gpio_init(1); // LED PIN NEED TO UPDATE TO IRL
+    gpio_init(17); // BUTTON PIN NEED TO UPDATE TO IRL
+    gpio_set_dir(1, GPIO_OUT);
+    gpio_set_dir(17, GPIO_IN);
     
     adc_init(); // init the adc module
     adc_gpio_init(26); // set ADC0 pin to be adc input instead of GPIO
     adc_select_input(0); // select to read from ADC0
 
 
-    gpio_put(12, 1);
-    while (!gpio_get(13)) {
+    gpio_put(1, 1);
+    while (gpio_get(17)) {
         sleep_ms(10);
     }
     char message[100];
+    gpio_put(1,0);
     while (1) {
         printf("Please enter a number of analog samples to take, from 1 to 100: \r\n ");
         scanf("%s", message);
         int number = atoi(message);
+        if (number > 100){
+            number = 100;
+        }
         for (int i = 0; i < number; i ++) {
             uint16_t result = adc_read();
             float voltage = result * 3.3f / 4095.0f;
